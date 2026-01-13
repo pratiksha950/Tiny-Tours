eda0d6561303b4e126e954759b98be9196d72817
pratiksha950 <salunkepratiksha950@gmail.com>
2026-01-13 21:22:07 +0530

    Middleware  added:

--- server/index.js
+++ server/index.js
@@ -13,11 +13,63 @@ app.use(cors());
 
 const PORT=process.env.PORT || 8080;
 
+const gatekepper=(req,res,next)=>{
+     const {name,isSociatyMember}=req.body;
+     console.log(`hellow ${name}`);
+
+     if(isSociatyMember){
+        next();
+     }else{
+        return res.json({
+            message:"access denied you must be society member"
+        })
+     } 
+}
+
+const areYouDrunk=(req,res,next)=>{
+    const {areYouDrunk}=req.body;
+
+    if(areYouDrunk){
+        return res.json({message:"entry denied for Drunk"})
+    }else{
+        next();
+    }
+}
+
+const ModakSociaty=(req,res)=>{
+    console.log("Inside  Modaksociaty");
+    const random=Math.round(Math.random()*100);
+
+    return res.json({
+        message:"thank you for visiting  Modaksociaty",
+        random
+    })
+}
+
+const ShamSociaty=(req,res)=>{
+    console.log("Inside Sham sociaty");
+    const random=Math.round(Math.random()*100);
+
+    return res.json({
+        message:"thank you for visiting sham sociaty",
+        random
+    })
+}
+
+
+
+app.post("/ModakSociaty",gatekepper,areYouDrunk,ModakSociaty);
+
+app.post("/ShamSociaty",gatekepper,ShamSociaty);
+
+
 app.get("/",(req,res)=>{
     res.json({
         message:"Welcome to server"
     })
 }) 
+
+
 
 app.get("/Health",(req,res)=>{
     res.json({Status:"OK"})
