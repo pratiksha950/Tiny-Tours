9fbe1bf8b754d1128d51d59b45ca8722d8c42c67
pratiksha950 <salunkepratiksha950@gmail.com>
2026-01-16 17:04:19 +0530

    routing setup completed:

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import {BrowserRouter,Route,Routes} from "react-router"
 
-createRoot(document.getElementById('root')).render(
-  <>
-    <h1>Tiny Tours</h1>
-  </>,
+const root =createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>}></Route>
+      <Route path="/about" element={<h1>About</h1>}></Route>
+      <Route path="/contact" element={<h1>Contact</h1>}></Route>
+    </Routes>
+  </BrowserRouter>,
 )
