5df46d07566c646f69f9b4a3ec7bc9fa9ad61ca3
pratiksha950 <salunkepratiksha950@gmail.com>
2026-01-16 23:15:30 +0530

    login functionality  added:

--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -1,15 +1,42 @@
-import {useEffect} from "react"
+import {useEffect, useState} from "react"
 import {setPageTitle} from "../utils.jsx"
-
-
+import Input from "../components/Input.jsx"
+import Button from "../components/Button.jsx"
+import axios from "axios"
 
 function Login() {
     useEffect(()=>{
         setPageTitle("Login-TinyTour")
     },[])
 
+    const [loginUser,setLoginUser]=useState({
+      email:"",
+      password:""
+    })
+
+    const checkUserLogin=async()=>{
+      const response=await axios.post("http://localhost:8080/login",loginUser)
+      console.log(response.data)
+    }
   return (
-    <div>Login</div>
+    <div className="w-60 flex flex-col justify-center items-center m-auto gap-4 ">Login
+      <Input 
+        type="email"
+        placeholder="Email"
+        value={loginUser.email}
+        onChange={(e)=>{setLoginUser({...loginUser,email:e.target.value})}}
+        />
+
+        <Input 
+        type="password"
+        placeholder="password"
+        value={loginUser.password}
+        onChange={(e)=>{setLoginUser({...loginUser,password:e.target.value})}}
+        />
+
+        <Button title="login" onClick={checkUserLogin}/>
+
+    </div>
   )
 }
 
